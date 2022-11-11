0c734bcd1f8e5c0339156f2faa39e751cddaecc1
wanderson-rpf <wandersonrpf@gmail.com>
2022-11-11 15:52:59 -0300

    :construction: [progress] Requisito 08 - Criando componente Timer

---
+++ src/components/Timer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 5,
+    };
+  }
+
+  componentDidMount() {
+    this.timerGame();
+  }
+
+  timerGame = () => {
+    const oneSecond = 1000;
+    const countTimer = setInterval(() => {
+      this.setState((prevState) => ({
+        count: prevState.count - 1,
+      }), () => this.verifyCount(countTimer));
+    }, oneSecond);
+  };
+
+  verifyCount = (timer) => {
+    const { count } = this.state;
+    if (count === 0) {
+      clearInterval(timer);
+    }
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <p>30 segundos para responder a pergunta:</p>
+        <p>{count}</p>
+      </div>
+    );
+  }
+}
+
+export default Timer;
