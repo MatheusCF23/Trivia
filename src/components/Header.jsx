cefd99c1df29b98df7d585cbed7e3203e9cde9c4
wanderson-rpf <wandersonrpf@gmail.com>
2022-11-12 16:14:24 -0300

    :rocket: [deploy] Requisito 17 concluído

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import md5 from 'crypto-js/md5';
 class Header extends React.Component {
   render() {
     const { name, score, email } = this.props;
-    console.log(this.props);
+    // console.log(this.props);
     const hash = md5(email).toString();
     return (
       <header>
--- src/tests/Feedback.test.js
+++ src/tests/Feedback.test.js
@@ -4,6 +4,8 @@ import Feedback from '../pages/Feedback';
 import App from '../App';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import userEvent from '@testing-library/user-event';
+import { tokenResponse } from './mocks/token';
+import { questionsResponse } from './mocks/questions';
 
 describe('Testes pagina de Feedback.', () => {
   it('1) Verifica se está na pagina de feedback.', () => {
@@ -25,10 +27,43 @@ describe('Testes pagina de Feedback.', () => {
     expect(nameUser).toBeInTheDocument();
   });
 
-  it('3) Verifica se na tela tem informação sobre o desempenho do player.', () => {
-    const { history } = renderWithRouterAndRedux(<App />)
-    act(() => {history.push('/feedback');})
+  it('3) Verifica se na tela tem informação sobre o desempenho do player.', async () => {
 
+    // <---------------- Mock do token e questions ---------------->
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValue(tokenResponse)
+    })
+
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValue(questionsResponse)
+    })
+    // <---------------- Mock do token e questions ---------------->
+
+    const { history } = renderWithRouterAndRedux(<App />)
+    const inputName = screen.getByTestId('input-player-name');
+    const inputEmail = screen.getByTestId('input-gravatar-email');
+    const buttonPlay =  screen.getByRole('button', { name: /Play/i });
+    expect(buttonPlay).toBeDisabled();
+    userEvent.type(inputName, 'teste');
+    userEvent.type(inputEmail, 'trybe@test.com');
+    expect(buttonPlay).toBeEnabled();
+    userEvent.click(buttonPlay);
+
+    // Timer para esperar o carregamento das questions na tela.
+    await new Promise((resolverOuter) => { setTimeout(resolverOuter, 250); });
+    
+    expect(history.location.pathname).toBe('/game')
+
+    // Passando pelas questions
+    for (let i = 0; i <= 4; i += 1 ) {
+      const corretAnswer = await screen.findByTestId('correct-answer');
+      userEvent.click(corretAnswer);
+      const btnNext = screen.getByTestId('btn-next');
+      userEvent.click(btnNext);
+    }
+    
     const feedbackGame = screen.getByTestId('feedback-text');
     expect(feedbackGame).toBeInTheDocument();
   });
